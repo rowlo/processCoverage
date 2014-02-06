5279342a50c368535b3fbc7579ab98682b1aa3ee
robert <robert@rowlo.de>
2014-02-06 15:55:26 +0100

    added parameters for complex project structure

--- processCoverage.sh
+++ processCoverage.sh
@@ -10,18 +10,17 @@
 # Robert Wloch - initial API and implementation
 ##############################################################################
 
-if [ ! $# -eq 1 ]; then
-  echo "usage: ${0} \"<file-pattern>\""
+if [ ! $# -eq 3 ]; then
+  echo "usage: ${0} <gcov-files-dir> \"<file-pattern>\" <target-dir>"
   exit 0
 fi
 
-lcov -d . -c -o coverage.info
+lcov -d ${1} -c -o ${1}/coverage.info
 
-lcov --list-full-path -e coverage.info ${1} -o coverage-stripped.info
+lcov --list-full-path -e ${1}/coverage.info ${2} -o ${1}/coverage-stripped.info
 
-genhtml -o html-coverage coverage-stripped.info
+genhtml -o ${3} ${1}/coverage-stripped.info
 
-lcov -d . -z
+lcov -d ${1} -z
 
 exit 0
-
